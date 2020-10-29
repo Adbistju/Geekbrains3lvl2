33f078ee372001c367c4ed281fda84f63cab7363
Adbistju <adjytant@gmail.com>
2020-10-29 21:01:43 +0300

    homeWork

--- src/BookNumber.java
+++ src/BookNumber.java
@@ -1,2 +1,25 @@
-public class Word {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BookNumber {
+    private Map<String, List<String>> bookNumber_hashMap = new HashMap<>();
+    private List<String> phone_number_list;
+
+    public void add(String surname, String phone_number) {
+        if (bookNumber_hashMap.containsKey(surname)) {
+            phone_number_list = bookNumber_hashMap.get(surname);
+            phone_number_list.add(phone_number);
+            bookNumber_hashMap.put(surname, phone_number_list);
+        } else {
+            phone_number_list = new ArrayList<>();
+            phone_number_list.add(phone_number);
+            bookNumber_hashMap.put(surname, phone_number_list);
+        }
+    }
+
+    public List<String> get(String surname) {
+        return bookNumber_hashMap.get(surname);
+    }
 }
